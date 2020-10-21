092fb15e1f3db1e6d49b795f0764e50badbde058
Shaneal Findley <sfindley899@gmail.com>
2020-10-21 13:33:43 -0400

    fix Procfile to get server running

--- server.js
+++ server.js
@@ -18,13 +18,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-/*const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
-const connection = mongoose.connection;
-connection.once('open', () => {
-	console.log("MongoDB database connection established successfully");
-})*/
+
+app.get('/', (req, res) => {
+	res.send("server running")
+})
 
 app.use((req, res, next) =>
 {
