e7897d08afb0a406b642b6e4b69cd7c4248342fa
Rami <rami2162@hotmail.com>
2020-11-22 12:55:33 -0500

    Fix server.js to server static content

--- seppi-project/server.js
+++ seppi-project/server.js
@@ -260,10 +260,10 @@ console.log(x);
 
 if (process.env.NODE_ENV === 'production')
 {
-	app.use(express.static('frontend/build'));
+	app.use(express.static('build'));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 	});
 }
 
