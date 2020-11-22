493e2f316225a8d9c02c1f27ee5cc7839fbc6b44
Rami <rami2162@hotmail.com>
2020-11-22 17:51:19 -0500

    Testing to see if new html is rendered on heroku app

--- seppi-project/server.js
+++ seppi-project/server.js
@@ -261,10 +261,10 @@ console.log(x);
 
 if (process.env.NODE_ENV === 'production')
 {
-  app.use(express.static(path.join(__dirname, "public")));
+  app.use(express.static(path.join(__dirname, "build")));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 
