d893656e7750ef4b95593ae615273b7652bbbde2
Rami <rami2162@hotmail.com>
2020-12-08 21:36:25 -0500

    Fix design

--- seppi-project/src/components/Recipe.js
+++ seppi-project/src/components/Recipe.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/Recipe.css';
+import RecipePreview from './RecipePreview';
 
 const Recipe = (props) => {
 	return (
